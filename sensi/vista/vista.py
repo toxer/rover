3708de5e5544667c797b579734b4a7e2c43ba913
Marco Tozzato <marco.tozzato@gmail.com>
2016-08-20 18:14:55 +0200

    Commit

--- sensi/vista/vista.py
+++ sensi/vista/vista.py
@@ -1,48 +1,83 @@
-import threading
 import picamera
+import subprocess
+import threading
 
 
-class Vista():
+class Vista(threading.Thread):
+   
     def __init__(self,inidirizzoTrasmissione,portaTrasmissione,handler):
+        threading.Thread.__init__(self)
         #Setto il sistema di trasmissione
         self.address = inidirizzoTrasmissione
         self.port=portaTrasmissione
         self.handler=handler
+       
+       
     
     def startTrasmission(self):
-        if self.gstreamer != None:
-            println("Trasmissione in corso")
+        if hasattr(self,'gstreamer'):
+            print("Trasmissione in corso")
             return
         if self.address==None or self.port==None:
             println("Parametri di connessione non settati")
             return
+
+        print("host "+self.address) 
         
         self.gstreamer = subprocess.Popen([
         'gst-launch-1.0', '-v',
         'fdsrc',
         '!', 'h264parse',
         '!', 'rtph264pay', 'config-interval=10', 'pt=96',
-        '!', 'udpsink', 'host='+self.address, 'port='+self.port	
-        ], stdin=subprocess.PIPE)
-        print ("Trasmissione partita")
+        '!', 'udpsink', 'host='+self.address, 'port='+self.port
+    ], stdin=subprocess.PIPE)
 
-    def startCamera(self,width,height):
+
+    def setCamera(self,width,height):
+        self.trasmissionOn=True
         if width==None:
             self.width = 1280
         else:
             self.width=width
         if height==None:
-                self.height = 1280
+                self.height = 720
         else:
             self.height=height
         
-        if self.camera==None:
+        if not hasattr (self,'camera'):
             self.camera = picamera.PiCamera(resolution=(self.width, self.height), framerate=25)
             self.camera.hflip = True
         if self.gstreamer == None:
-            print("Trasmissioe non ancora partita")
+            print("Trasmissione non ancora partita")
             return
-        self.camera.start_recording(self.gstreamer.stdin, format='h264', bitrate=2000000)
+        else:
+            print("Stream agganciato")
+    
+    def run(self):
+        self.startTrasmission();
+        self.setCamera(None,None);
+        self.trasmissionOn=True
+
+        
+
+        if hasattr(self,'camera') and hasattr(self,'gstreamer'):
+            self.camera.start_recording(self.gstreamer.stdin, format='h264', bitrate=2000000)
+            while self.trasmissionOn:
+                pass
+
+            
+  
+    
+    def stop(self):
+        self.trasmissionOn=False
+        if hasattr(self,'gstreamer'):
+            pass
+        
+        
+
+      
+
+
 
         
 
--- test/testVista.py
+++ test/testVista.py
@@ -1,9 +1,12 @@
 import sys  
 sys.path.append("../sensi/vista")  
 import vista
+import time
+
 
 
 #attivo la trasmissione
-vista = vista.Vista("192.168.1.3","5000",None)
-vista.startTrasmission()
-vista.startCamera()
+vista = vista.Vista("192.168.1.7","5000",None)
+vista.start()
+
+#vista.stop();
